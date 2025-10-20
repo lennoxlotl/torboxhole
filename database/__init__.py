a3d0623c31a8962d29bf160bf35c60358e553759
lennoxlotl <admin@lennoxlotl.dev>
2025-10-20 22:34:45 +0200

    feat: base structure for rust version

--- database/__init__.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import declarative_base, sessionmaker
-
-from config import config
-
-DB_ENGINE = create_engine(f'sqlite:///{config.database_path}')
-
-Base = declarative_base()
-Session = sessionmaker(bind=DB_ENGINE)
---
+++ src/main.rs
@@ -0,0 +1,6 @@
+pub mod torbox;
+
+#[tokio::main]
+async fn main() {
+    print!("grr");
+}
